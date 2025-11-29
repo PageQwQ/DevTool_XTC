e5c24dbee039765d3e3799aaaf8cb6a42e44265e
nullXC <zuanguan13635255131@outlook.com>
2025-11-29 15:38:40 +0800

    Initial Commit

---
+++ DevTool_XTC/DevTool_XTCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevTool_XTCApp.swift
+//  DevTool_XTC
+//
+//  Created by PageQwQ on 2025/11/29.
+//
+
+import SwiftUI
+
+@main
+struct DevTool_XTCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
